1ce21139d92188e2669f96ba1c1b8842e4181f67
Clement Farabet <clement.farabet@gmail.com>
2011-09-25 00:12:06 -0400

    More help.

--- lua/init.lua
+++ lua/init.lua
@@ -50,40 +50,67 @@ local help = {
 This function creates a new graphical model. Here is a simple
 example. Say we want to create a graph with 4 variables
 x1, x2, x3 and x4. The first 3 variables are binary, and the
-fourth is tertiary. We first define a state table:
+fourth is tertiary. We define the variables like this:
 
-> states = {2, 2, 2, 3}
+> variables = {{'x1',2}, {'x2',2}, {'x3',2}, {'x4',3}}
 
-Then we want to define 6 factors, 4 unary factors, and 2 Potts
+(note: binary variables are the default, so {'x1',2} can be 
+ replaced by 'x1', for simplicity)
+
+Then we need to define the factors of the graph.
+Say we want to define 6 factors, 4 unary factors, and 2 Potts
 factors. The table goes like this:
 
-> factors = {{1}, {2}, {3}, {4}, {1,2}, {1,4}}
+> factors = {{func1, {1}}, {func2, {2}}, {func3, {3}}, {func4, {4}}, 
+             {func5, {1,2}}, {func6, {1,4}}}
 
 Each entry of this table defines a new factor, which takes for
-arguments the variables listed. The 5th factor of the list is
-noted f(x1,x2).
+arguments the variables listed. The first part of each tuple
+is a lua function that was defined apriori.
 
-Lastly, we need to specify which all possible energies for each
-factor. Energies are real-valued, an energy of 0 represents a
-likely configuration, whereas a large energy represents an 
-unlikely configuration.
+Such a function is simple to define: it must return an energy for each 
+configuration of its inputs. For instance, func5 could be defined as:
 
-> energies = {{0,0}, {0,100}, {100,0}, {0,40}, {0,100,0,0}, {100,100,0,0}}
+func5 = function(x1,x2) 
+           if x1 == 1 and x2 == 0 then return 1e3
+           else return 0 end
+        end
 
-In that example, x2 is likely to be 0 (low energy) and very unlikely
-to be 1 (high energy). On the other hand, x1 is agnostic, having a
-low energy for both 0 and 1.
-For multi-term factors, like f(x1,x2), the energies are listed in the
-same order they would be on a boolean table:
+That function assigns a high cost to (x1,x2) == (1,0), and 0-costs to
+all other configurations.
 
-x1 | x2 | f(x1,x2)
- 0    0    0
- 0    1    100
- 1    0    0
- 1    1    0
+(note: the table opengm.factors provides a couple of standard factor
+ functions)
 
-Finally, the graph can be obtained like this:
-> graph = opengm.Graph(states, factors, energies)    ]]
+The graph can be obtained like this:
+> graph = opengm.Graph(variables, factors)    ]],
+
+optimize = [[
+This function optimizes the current graphical model (inference). Given
+a graph g, a call to g:optimize{} fills the states vector with the
+optimal states obtained. These can be retrieved like this:
+states = g:getstates().
+
+Several inference algorithms are available:
+- A* (a*)
+- Belief Propagation (bp)
+- Tree-Reweighted Belief Propagation (trbp)
+- Iterated Conditional Modes (icm)
+- Lazy Flipper (lf) 
+
+Once optimized, printing a graph reveals its optimal states:
+> g:optimize{}
+> print(g)
+<opengm.Graph>
+  + nb of variables: 4
+  + nb of factors: 6
+  + graph is acyclic
+  + current (optimized) variable states: 
+    - x1 [0]
+    - x2 [0]
+    - x3 [0]
+    - x4 [1]
+]]
 }
 
 ----------------------------------------------------------------------
@@ -178,7 +205,7 @@ local function optimize(graph, ...)
    -- usage
    local _, method, iterations, verbose = xlua.unpack(
       {...},
-      'opengm.Graph:optimize', 'optimize a graph (perform inference), ',
+      'opengm.Graph:optimize', help.optimize,
       {arg='method', type='string', help='optimization method: a* | bp | trbp | lf | icm', default='bp'},
       {arg='verbose', type='boolean', help='verbose'},
       {arg='iterations', type='number', help='max number of iterations (bp & trbp)', default=100},
